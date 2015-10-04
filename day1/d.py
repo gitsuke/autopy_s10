cb8a8118602cae26064ffd66636ee504038b5199
gitsuke <gitsuke@outlook.com>
2015-10-04 17:04:09 +0800

    finish ligin_interface.py and drawing

--- day1/d.py
+++ day1/d.py
@@ -1,6 +1,30 @@
+# -*- coding:utf-8 -*-
+
 user_dict = {'test':{'passwd':'test','flag':'unlock'},
              'user':{'passwd':'user','flag':'unlock'}}
 if (user_dict.get('test') and
         user_dict['test']['passwd'] == 'test' and
         user_dict['test']['flag'] == 'unlock'):
     print 'login'
+'''
+import cPickle as pickle
+count_lock = 0
+with open('account.pkl') as f:  #载入用户账户密码信息
+    user_dict = pickle.load(f)
+while True:
+    if count_lock == 5:   #5次出错，锁定账号
+        with open('account.pkl','w') as f:
+            for key in user_dict:
+                user_dict[key]['flag'] = 'lock'
+            print user_dict
+            user_dict = pickle.dump(user_dict, f)
+        print 'All users are locked, please contact your administrator to unlock.'
+        break
+    if count_lock < 5:
+        count_lock += 1
+        print count_lock
+
+with open('account.pkl') as f:
+    user_dict = pickle.load(f)
+print user_dict
+'''
--- day1/login_interface.py
+++ day1/login_interface.py
@@ -1,32 +1,73 @@
 # -*- coding:utf-8 -*-
 import cPickle as pickle
+import random
 
-lock_flag = False   #初始化锁定的状态为false
-
+exit_flag = False   #初始化锁定的状态为false
+count_lock = 0
+print 'Welcome to login system!'
+print '****************************'
+with open('account.pkl') as f:  #载入用户账户密码信息
+    user_dict = pickle.load(f)
 while True:
-    print '''Welcome to login system!
-            1. login system
-            2. exit system
-            *************************'''
-    get_input = raw_input('please input your choise number: ').strip()
-    if get_input == '':continue
-    elif get_input == '1':
-        #载入用户账户密码信息
-        with open('account.pkl') as f:
-            user_dict = pickle.load(f)
-
-        user = raw_input('please input your user: ').strip()
-        if user == '':continue
-        passwd = raw_input('please input your passwd: ').strip()
-        if passwd == '':continue
-        #判断输入的用户名与密码已经是否被锁定
-        if (user_dict.get(user) and
-                user_dict[user]['passwd'] == passwd and
-                user_dict[user]['flag'] == 'unlock'):
-            print 'Welcome %s to here:' % user
-
-    elif get_input == '2':
-        print 'good bye!'
+    if count_lock == 5:   #5次出错，锁定账号
+        with open('account.pkl','w') as f:
+            for key in user_dict:
+                user_dict[key]['flag'] = 'lock'
+            user_dict = pickle.dump(user_dict, f)
+        print '******************************************************************'
+        print 'All users are locked, please contact your administrator to unlock.'
         break
+
+    user = raw_input('please input your user: ').strip()
+    if user == '':continue
+    passwd = raw_input('please input your passwd: ').strip()
+    if  user_dict.get(user) and user_dict[user]['flag'] == 'lock':
+        print 'The %s is lock, please contact your administrator to unlock.' % user
+        break
+    #判断输入的用户名是否被锁定，并验证密码
+    if (user_dict.get(user) and
+            user_dict[user]['flag'] == 'unlock' and
+            user_dict[user]['passwd'] == passwd):
+        while True:
+            print '***********************************************'
+            guess_choise = raw_input('Do you start playing guess numbr? (yes/no): ').strip()
+            if guess_choise == '':continue
+            elif guess_choise == 'yes':
+                print 'The number range is 1 - 10.'
+                real_num = random.randrange(1,11)
+                retry_count = 0
+                while retry_count < 3:  #有3次机会猜数字
+                    guess_num = raw_input('please guess the real number: ').strip()
+                    if guess_num == '':continue
+                    if guess_num.isdigit():
+                        guess_num = int(guess_num )
+                        if guess_num > real_num :
+                            print 'Wrong! you need try smaller!'
+                        elif guess_num < real_num :
+                            print 'Wrong! you need try bigger!'
+                        else:
+                            print 'Congratulation! you got it.'
+                            break
+                    else:
+                        print 'Please input a number,try again!'
+                        continue
+
+                    retry_count +=1
+                else:
+                    print '---------------------------------'
+                    print 'The real number is ', real_num
+
+            elif guess_choise == 'no':
+                exit_flag = True
+                break
+            else:
+                print 'Please input "yes" or "no",try again.'
+                continue
+
+    if exit_flag:break
     else:
-        print 'your chiose incorrect,try again!'
+        count_lock += 1
+        if (5 - count_lock):
+            print 'The user or passwd is incorrect, you have %s times try again!' %  (5-count_lock)
+
+print 'Bye bye!'
